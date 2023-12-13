fed0ae11737571415fa2e440745ec6ef414167bb
Sebastian Betancourth <betancourth.ingenieria@gmail.com>
2023-12-12 23:41:06 -0500

    fix:agregar puerto de ejecución de la aplicacion

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(books_router)
+app.include_router(books_router)dock
 app.include_router(auth_router)
 
 
 if __name__ == '__main__':
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
